4eb700e0c574a361bfd8c478cf153bc0511891ca
Jimmy Gregerson <jimmygregerson@gmail.com>
2016-02-01 01:40:21 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShowSwap::Application.config.secret_key_base = 'c44283c4b9e7b3643b0f81868c778bd0de32054e35b7f526f4d158f37314dad2d37de0d6639479dd060f13d86abbc4eee5dcaa8b1debf302af4770d023ae7b0e'
